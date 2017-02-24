91dd81f5ac11c4cab7eced75c8e5d8e242ac8348
Xindi LI <xil573@eng.ucsd.edu>
2017-02-24 12:19:54 -0800

    fix bug

--- public/js/SearchResult.js
+++ public/js/SearchResult.js
@@ -1,25 +1,34 @@
 ﻿function backToAddNewContactPage() {
-    window.location.href = "add-new-account";
+    //window.location.href = "add-new-account";
+    var url = window.location.href;
+    console.log(url);
+    var index = url.indexOf("ount/");
+    console.log(index);
+    //var idStr = url.substring(index + 5);
+    console.log(idStr);
+    //window.location.href = "/?id=" + idStr;
+    var idStr = index + 5;
+    window.location.href = "/?id=" + idStr;
 }
 
 function selectInfoToSend() {
     window.location.href = "select_info";
 }
 
-$.getJSON("people.json", function (data) {
-    console.log(data);
-    var i;
-    var url = window.location.href;
-    var index = url.indexOf("?name=");
-    var name = url.substring(index + 6);
-    for (i = 0; i < data.person.length; i++) {
-        if (data.person[i].name.toLowerCase().includes(name.toLowerCase())) {
-            $("#result_list").append(
-                '<li class="contact_item"><button id="' + data.person[i].id + '" onclick="requestAddToContact(this)"><img  class="contact_img" src="' + data.person[i].imageURL + '"><strong>' + data.person[i].name + '</strong></button></li>'
-            );
-        }
-    }
-});
+//$.getJSON("people.json", function (data) {
+//    console.log(data);
+//    var i;
+//    var url = window.location.href;
+//    var index = url.indexOf("?name=");
+//    var name = url.substring(index + 6);
+//    for (i = 0; i < data.person.length; i++) {
+//        if (data.person[i].name.toLowerCase().includes(name.toLowerCase())) {
+//            $("#result_list").append(
+//                '<li class="contact_item"><button id="' + data.person[i].id + '" onclick="requestAddToContact(this)"><img  class="contact_img" src="' + data.person[i].imageURL + '"><strong>' + data.person[i].name + '</strong></button></li>'
+//            );
+//        }
+//    }
+//});
 function requestAddToContact(elem) {
     var $buttonClicked = $(elem);
     var id = $buttonClicked.attr("id");
--- public/js/select-info.js
+++ public/js/select-info.js
@@ -1,5 +1,15 @@
 function backToAddNewContactPage() {
-    window.location.href = "add-new-account";
+    //window.location.href = "add-new-account";
+    //window.location.href = "add-new-account";
+    var url = window.location.href;
+    console.log(url);
+    var index = url.indexOf("ount/");
+    console.log(index);
+    //var idStr = url.substring(index + 5);
+    console.log(idStr);
+    //window.location.href = "/?id=" + idStr;
+    var idStr = index + 5;
+    window.location.href = "/?id=" + idStr;
 }
 
 function confirmPage() {
