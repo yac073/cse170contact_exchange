586b3b9e8bd725fd10d659c563dddd2984d183d9
Xindi LI <xil573@eng.ucsd.edu>
2017-02-23 19:51:24 -0800

    edit profile

--- public/js/MyProfile.js
+++ public/js/MyProfile.js
@@ -1,4 +1,53 @@
-﻿function backToIndex() {
+﻿
+$(document).ready(function() {
+	initializePage();
+})
+
+/*
+ * Function that is called when the document is ready.
+ */
+function initializePage() {
+
+	$("#phone").click(showPhone);
+	$("#email").click(showEmail);
+	$("#fb").click(showFB);
+  
+  }
+function showPhone(event){
+    $(this).append("<p>123-456-789</p>");
+    var btn_pub = document.createElement("BUTTON");        // Create a <button> element
+    var btn_del = document.createElement("BUTTON");
+	var t = document.createTextNode("Public");       // Create a text node
+	var s = document.createTextNode("Delete");
+	btn_pub.appendChild(t);  
+	btn_del.appendChild(s);
+
+	var foo = document.getElementById("phone");
+	foo.appendChild(btn_pub); 
+	foo.appendChild(btn_del);
+
+  }
+
+function showEmail(event){
+	$(this).append("<p>xxx@ucsd.edu</p>");
+	var btn_pub = document.createElement("BUTTON");        // Create a <button> element
+    var btn_del = document.createElement("BUTTON");
+	var t = document.createTextNode("Public");       // Create a text node
+	var s = document.createTextNode("Delete");
+	btn_pub.appendChild(t);  
+	btn_del.appendChild(s);
+
+	var foo = document.getElementById("email");
+	foo.appendChild(btn_pub); 
+	foo.appendChild(btn_del);
+
+}
+
+function showFB(event){
+	$(this).append("<a href=https://www.facebook.com/><p> link to FB</p></a>");
+}
+
+function backToIndex() {
     window.location.href = "/";
 }
 
